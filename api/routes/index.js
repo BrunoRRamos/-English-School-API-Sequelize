bd599708293868be6a5a9fa97eae51d1377d12af
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-01-31 11:42:09 -0300

    Pessoa Route Done

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,10 @@
 const bodyParser = require('body-parser');
+const pessoasRoute = require('./pessoasRoute.js')
 
 //Cria uma rota base
 module.exports = app => {
     app.use(bodyParser.json());
+    app.use(pessoasRoute);
     app.get('/', (req, res) => {
         res.send('Batata');
     })
