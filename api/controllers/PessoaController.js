6de6c67ac24b25fb70cd4f9042c63652e8107687
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-02-18 10:39:21 -0300

    feat: New routes (getPessoaById, createPessoa)

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -4,13 +4,38 @@ class PessoasController {
     //Método GET que lista todas as Pessoas cadastradas no banco.
     static async listPessoas(req, res) {
         try {
-            const allPeople = await database.Pessoas.findAll();
-            return res.status(200).json(allPeople);
+            const todasPessoas = await database.Pessoas.findAll();
+            return res.status(200).json(todasPessoas);
 
         } catch(error) {
             return res.status(500).json(error.menssage);
         }
     }
+
+    static async getPessoaById (req, res) {
+        const { id } = req.params;
+
+        try {
+            const umaPessoa = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(umaPessoa);
+
+        } catch (error) {
+            return res.status(500).json(error.menssage);
+        }
+    }
+
+    static async createPessoa (req, res) {
+        const novaPessoaDados = req.body;
+        
+        try {
+            const novaPessoa = await database.Pessoas.create(novaPessoaDados);
+            return res.status(201).json(novaPessoa);
+            
+        } catch (error) {
+            return res.status(500).json(error.menssage);
+        }
+    }
+
 }
 
 module.exports = PessoasController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -4,5 +4,7 @@ const PessoasController = require('../controllers/PessoaController');
 const router = Router();
 
 router.get('/pessoas', PessoasController.listPessoas);
+router.get('/pessoas/:id', PessoasController.getPessoaById);
+router.post('/pessoas', PessoasController.createPessoa);
 
 module.exports = router;
