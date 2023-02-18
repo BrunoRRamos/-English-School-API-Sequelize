6e0531dcbd8e9132d82a4b5d5b44c912a8128e67
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-02-18 17:57:13 -0300

    feat: Routes (updatePessoa and deletePessoa)

--- api/controllers/PessoaController.js
+++ api/controllers/PessoaController.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 const database = require('../models');
 
 class PessoasController {
@@ -36,6 +37,31 @@ class PessoasController {
         }
     }
 
+    static async updatePessoa (req, res) {
+        const novasInfos = req.body;
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.update(novasInfos, { where: { id: Number(id) } });
+            const pessoaAtualizada = await database.Pessoas.findOne({ where: { id: Number(id) } });
+            return res.status(200).json(pessoaAtualizada);
+
+        } catch (error) {
+            return res.status(500).json(error.menssage);
+        }
+    }
+
+    static async deletePessoa (req, res) {
+        const { id } = req.params;
+
+        try {
+            await database.Pessoas.destroy({ where : { id: id }});
+            return res.status(200).json({ Message: `Registro de ID:${id} deletado !`})
+
+        } catch (error) {
+            return res.status(500).json(error.menssage);
+        }
+    }
 }
 
 module.exports = PessoasController;
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -6,5 +6,7 @@ const router = Router();
 router.get('/pessoas', PessoasController.listPessoas);
 router.get('/pessoas/:id', PessoasController.getPessoaById);
 router.post('/pessoas', PessoasController.createPessoa);
+router.put('/pessoas/:id', PessoasController.updatePessoa);
+router.delete('/pessoas/:id', PessoasController.deletePessoa);
 
 module.exports = router;
